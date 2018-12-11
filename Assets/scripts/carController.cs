fb2902285857a0a182a5d1aac19e28d5989c3dce
HarshitaAgrawal2 <harshita.agrawal@somaiya.edu>
2018-12-11 13:15:17 +0530

    Add files via upload

---
+++ Assets/scripts/carController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carController : MonoBehaviour {
+
+    public float carSpeed;
+    Vector3 position;
+    public float maxPos = 2.2f;
+
+    // Use this for initialization
+    void Start () {
+		position = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        position.x += Input.GetAxis("Horizontal") * carSpeed * Time.deltaTime;
+        position.x = Mathf.Clamp(position.x, -2.2f, 2.2f);
+        transform.position = position;
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "EnemyCar")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
